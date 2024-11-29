478d76227e0bd754d38a09d9c0ef4fb1ab437730
Ritesh Giri <rrgiri032@gmail.com>
2024-11-29 20:28:55 +0530

    Edit App.jsx

---
+++ src/components/Pages/index.js
@@ -0,0 +1,7 @@
+import About from "./About";
+import HeroPortfolio from "./HeroPortfolio";
+import Iphone from "./Iphone";
+import Projects from "./Projects";
+import Technologies from "./Technologies";
+
+export { About, HeroPortfolio, Iphone, Projects, Technologies };
